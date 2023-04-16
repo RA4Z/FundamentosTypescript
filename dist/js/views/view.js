de9251b83e2ce2a80c6dd0ffc5ea636a187ec527
RA4Z <rraz639@gmail.com>
2023-04-16 16:32:50 -0300

    Removendo comentários do js

--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,5 +1,4 @@
 export class View {
-    //escapar está como opcional aqui, adiciona ? para se tornar opcional
     constructor(seletor, escapar) {
         this.escapar = false;
         this.element = document.querySelector(seletor);
@@ -10,7 +9,6 @@ export class View {
     update(model) {
         let template = this.template(model);
         if (this.escapar) {
-            //Expressão regular que remove a tag Script
             template = template.replace(/<scipt>[\s\S]*?<\/script>/, '');
         }
         this.element.innerHTML = template;
