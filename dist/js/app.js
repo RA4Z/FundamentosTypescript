a35b33f8b9b0e7476b322135436ecde18fa74a7a
RA4Z <rraz639@gmail.com>
2023-04-18 12:27:44 -0300

    Adicionando conexão GET com API

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,3 +10,12 @@ if (form) {
 else {
     throw Error('Não foi possível inicializar a aplicação. verifique se o form está nulo');
 }
+const botaoImporta = document.querySelector('#botao-importa');
+if (botaoImporta) {
+    botaoImporta.addEventListener('click', () => {
+        controller.importaDados();
+    });
+}
+else {
+    throw Error('Botão importa não foi encontrado');
+}
