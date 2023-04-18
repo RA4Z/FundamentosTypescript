7c9a99ed1aeaae1c3b82964748fb7ba606b92f49
RA4Z <rraz639@gmail.com>
2023-04-17 21:20:33 -0300

    Adicionando dom-injector

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { domInjector } from "../decorators/dom-injector.js";
 import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 import { DiasDaSemana } from "../enums/dias-da-semana.js";
@@ -8,13 +9,13 @@ import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
 
-    @domInject('#data')
+    @domInjector('#data')
     private inputData: HTMLInputElement;
 
-    @domInject('#quantidade')
+    @domInjector('#quantidade')
     private inputQuantidade: HTMLInputElement;
 
-    @domInject('#valor')
+    @domInjector('#valor')
     private inputValor: HTMLInputElement;
 
     private negociacoes = new Negociacoes();
